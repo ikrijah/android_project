4e4b8f8d7728e5b003852e480db1819f4236ca92
ikrijah <krijah.iptissame@hotmail.fr>
2020-01-20 18:20:27 +0100

    2eme version : LifeCycleActivity ajouté

--- app/src/main/java/fr/isen/krijah/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/krijah/androidtoolbox/LoginActivity.kt
@@ -1,11 +1,16 @@
 package fr.isen.krijah.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        private const val GOOD_ID = "admin"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +19,10 @@ class LoginActivity : AppCompatActivity() {
         loginAction.setOnClickListener {
             val identifiant = loginUser.text.toString()
             val mdp = password.text.toString()
-            if (identifiant.equals("admin") and mdp.equals("123")) {
+            if (identifiant == GOOD_ID && mdp.equals("123")) {
                 val message = "tu as bien été identifié $identifiant"
                 Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                startActivity( Intent (this@LoginActivity, HomeActivity::class.java))
             }
         }
     }
