16dc4cebff10bafb13f20d0d3ffbda3958a247ca
ikrijah <krijah.iptissame@hotmail.fr>
2020-01-21 15:48:19 +0100

    Sauvegarde ajoutée On reste connecté si on ferme l'application et on se déconnecte que lorsque l'on clique sur "deconnexion"

---
+++ app/src/main/java/fr/isen/krijah/androidtoolbox/HomeActivity.kt
@@ -0,0 +1,40 @@
+package fr.isen.krijah.androidtoolbox
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+
+        lifeCycleImageView.setOnClickListener {
+            startActivity( Intent (this@HomeActivity, LifeCycleActivity::class.java))
+        }
+
+        buttonDeconnect.setOnClickListener {
+            val message = "tu as bien été déconnecté"
+
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            startActivity( Intent (this@HomeActivity, LoginActivity::class.java))
+
+            val logInput: SharedPreferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE)
+
+            val editor: SharedPreferences.Editor = logInput.edit()
+
+            editor.clear()
+
+            editor.apply()
+        }
+
+    }
+
+}
