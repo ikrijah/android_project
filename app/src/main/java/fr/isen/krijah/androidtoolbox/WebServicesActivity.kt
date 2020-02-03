c11edb2a3a4f06f7cf60f101577fa86be6ab16e3
ikrijah <krijah.iptissame@hotmail.fr>
2020-02-03 16:00:15 +0100

    APK

--- app/src/main/java/fr/isen/krijah/androidtoolbox/WebServicesActivity.kt
+++ app/src/main/java/fr/isen/krijah/androidtoolbox/WebServicesActivity.kt
@@ -3,10 +3,15 @@ package fr.isen.krijah.androidtoolbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class WebServicesActivity : AppCompatActivity() {
+    val url = "https://randomuser.me/api/?format=json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_services)
+
+
+
     }
 }
